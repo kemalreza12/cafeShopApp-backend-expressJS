9217193433bfad088894bf6b8633b2dce9ff7146
kemalreza12 <kemalreza92@gmail.com>
2020-10-15 21:06:48 +0700

    readme

---
+++ README.md
@@ -0,0 +1,63 @@
+# cafeShopApp-backend-expressJS
+
+![GitHub repo size](https://img.shields.io/github/repo-size/kemalreza12/cafeShopApp-backend-expressJS)
+
+## General Info
+cafeShopApp-backend-expressJS is a backend for dicey-CaffeApp frontend.
+
+## Built With
+* [Node.js](https://nodejs.org/en/)
+* [Express.js](https://expressjs.com/)
+
+## Requirements
+* [Node.js](https://nodejs.org/en/)
+* [Database](https://github.com/kemalreza12/cafeShopApp-backend-expressJS/blob/main/dbcaffe.sql)
+* [Postman](https://www.postman.com/) for Testing
+
+## NPM Package
+* [Bcryptjs](https://www.npmjs.com/package/bcryptjs)
+* [Body-parser](https://www.npmjs.com/package/body-parser)
+* [Cors](https://www.npmjs.com/package/cors)
+* [Dotenv](https://www.npmjs.com/package/dotenv)
+* [Express](https://www.npmjs.com/package/express)
+* [Jsonwebtoken](https://www.npmjs.com/package/jsonwebtoken)
+* [Morgan](https://www.npmjs.com/package/morgan)
+* [Multer](https://www.npmjs.com/package/multer)
+* [Mysql](https://www.npmjs.com/package/mysql)
+* [Redis](https://www.npmjs.com/package/redis)
+
+
+## Installation
+
+Clone this repository and then use the package manager npm to install dependencies.
+```
+npm install
+```
+
+## Setup .env example
+
+Create or edit .env file in your root project folder.
+
+```env
+
+DB_HOST = localhost
+DB_USER = your_database_username
+DB_PASSWORD = your_database_password
+DB_DATABASE = dbcaffe
+PORT = 8000
+SECRET_KEY = your_secret_key
+
+```
+
+### Run project for development
+```
+npm run dev
+```
+
+### API documentation link
+```
+See [Documentation API](https://www.getpostman.com/collections/cad45b669302e594c688)
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/
--- dbcaffe.sql
+++ dbcaffe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 16, 2020 at 02:48 AM
+-- Generation Time: Oct 14, 2020 at 08:17 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -48,18 +48,19 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 
 CREATE TABLE `histories` (
   `id` int(11) NOT NULL,
-  `income` varchar(32) NOT NULL,
-  `orders` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `invoices` varchar(64) NOT NULL,
+  `cashier` varchar(32) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `orders` varchar(64) NOT NULL,
+  `amount` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `histories`
 --
 
-INSERT INTO `histories` (`id`, `income`, `orders`, `time`) VALUES
-(1, 'Rp. 800.000', 35, '2020-08-05 03:23:15'),
-(2, 'Rp. 500.000', 26, '2020-08-11 20:58:20');
+INSERT INTO `histories` (`id`, `invoices`, `cashier`, `date`, `orders`, `amount`) VALUES
+(3, '#10928', 'Cashier 1', '2020-10-09 03:13:22', 'ice tea', '120000');
 
 -- --------------------------------------------------------
 
@@ -82,12 +83,48 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `image`, `price`, `idCategory`, `createdAt`, `updateAt`) VALUES
-(1, 'kopi Espresso', 'www.google.com/espresso.jpg', 15000, 1, '2020-08-13 06:52:35', '2020-08-13 06:52:35'),
-(2, 'Coffe Latte', 'www.google.com/colatte.jpg', 15000, 1, '2020-08-06 00:22:00', '2020-08-12 20:44:22'),
-(3, 'Cappucino', 'www.caffete.com/cappucino.jpg', 5000, 1, '2020-08-10 00:25:52', '2020-08-12 20:44:49'),
-(4, 'Choco Rhum', 'www.google.com/choco.jpg', 28000, 2, '2020-08-10 00:28:32', '2020-08-12 20:45:21'),
-(8, 'Chiken katsu', 'chik.jpg', 10000, 2, '2020-08-13 06:16:18', '2020-08-13 06:16:18'),
-(9, 'Chiken katsudon', 'chik.jpg', 10000, 2, '2020-08-13 06:52:05', '2020-08-13 06:52:05');
+(1, 'kopi Espresso', 'http://localhost:8000/uploads/2020-09-02T01_31_05.018Zjeremy-ricketts-6ZnhM-xBpos-unsplash.png', 10000, 1, '2020-09-07 23:48:44', '2020-09-07 23:48:44'),
+(2, 'Coffe Latte', 'http://localhost:8000/uploads/2020-09-02T01_31_35.711Zbear.png', 15000, 1, '2020-10-02 21:08:09', '2020-10-02 21:08:09'),
+(32, 'Chiken Katsu', 'http://localhost:8000/uploads/2020-09-09T08_09_28.930Zchickenkatsu.png', 20000, 2, '2020-09-09 10:14:31', '2020-09-09 10:14:31'),
+(33, 'Wiener', 'http://localhost:8000/uploads/2020-09-09T08_10_11.837Zwiener.png', 25000, 2, '2020-09-09 08:10:11', '2020-09-09 08:10:11'),
+(34, 'Black Forest', 'http://localhost:8000/uploads/2020-09-09T08_10_48.004Zblackforest.png', 30000, 2, '2020-09-09 08:10:48', '2020-09-09 08:10:48'),
+(35, 'Choco Rhum', 'http://localhost:8000/uploads/2020-09-09T08_11_18.646Zchocorum.png', 35000, 2, '2020-10-07 23:17:29', '2020-10-07 23:17:29'),
+(39, 'ref', 'http://localhost:8000/uploads/2020-10-06T02_29_19.738Z548449fe70f5b6c24176150760ca8a8a.jpg', 123, 1, '2020-10-06 02:29:19', '2020-10-06 02:29:19'),
+(41, 'coffe', 'http://localhost:8000/uploads/2020-10-08T00_23_27.539Zbear.png', 25000, 1, '2020-10-08 00:23:27', '2020-10-08 00:23:27'),
+(42, 'wiener', 'http://localhost:8000/uploads/2020-10-09T06_42_26.933Zwiener.png', 40000, 1, '2020-10-09 06:42:27', '2020-10-09 06:42:27'),
+(44, 'Red Velvet', 'http://localhost:8000/uploads/2020-10-09T06_51_53.015Zredvelvet.png', 25000, 1, '2020-10-09 06:51:53', '2020-10-09 06:51:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `roleId` int(11) NOT NULL,
+  `firstName` varchar(32) NOT NULL,
+  `lastName` varchar(32) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `roleId`, `firstName`, `lastName`, `createdAt`, `updatedAt`) VALUES
+(1, 'kemalr@gmail.com', '$2a$10$jZ0Ew.BG6rBPQXP7YO8WlOTB/h0vLb8M2uXyktNLvvM8yL3CQvbiO', 2, 'kemal', 'reza', '2020-08-23 10:32:00', '2020-08-23 10:32:00'),
+(2, 'kemalreza@gmail.com', '$2a$10$8lKzJ0R6z6UW/j2LfuLinuUe7vKzvGcmWGQJ.Dwa/rAOcUmAvgMV.', 2, 'kemal', 'reza', '2020-08-24 03:50:14', '2020-08-24 03:50:14'),
+(3, 'kreza@gmail.com', '$2a$10$.8pQX/J0uwNm1eJ7OI9jq.N6h64YR6PJznPFX3oZ2AVa/8UaX.Etq', 2, 'kemal', 'reza', '2020-08-24 07:23:20', '2020-08-24 07:23:20'),
+(6, 'check@gmail.com', '$2a$10$onIdWDjKE.YhJfyOVpiTVeenFD2JXgOZNxiw4iRU.NTAkCZgGZpNq', 2, 'misa', 'misa', '2020-09-08 04:36:25', '2020-09-08 04:36:25'),
+(9, 'example@gmail.com', '$2a$10$O4gqmO33ZEW2LlkqHgnW7OpVJIrlBx.KEsZF7Egdv3V7IJkhsk8bi', 2, 'da', 'dada', '2020-09-09 07:57:41', '2020-09-09 07:57:41'),
+(11, 'm.kurisu@channel.com', '$2a$10$KWEOP2D7ilsxKDFFmiWGiubZG1aTDAZUKclbPvcfScMSblzNy5H0m', 2, 'Makise', 'Kurisu', '2020-09-09 10:04:31', '2020-09-09 10:04:31'),
+(13, 'example2@gmail.com', '$2a$10$vGKLZUjso/X4xsNP7SqdbeGOq2TzF.S4qyjoUAE7zRErm0CN3Bl/u', 2, 'kjl', 'r', '2020-09-24 07:47:35', '2020-09-24 07:47:35'),
+(14, 'okarin@gmail.com', '$2a$10$YoTxMj5NtZkZVYZ.e3aQiuoNig5E2tQtiZ588SJkL8kUnv46fV8JG', 2, 'okabe', 'rintarou', '2020-10-02 21:05:35', '2020-10-02 21:05:35'),
+(17, 'example@gmail.coms', '$2a$10$m/zvNfmxCvkq2EMW.EO5peI1KvilNXG0yHePp1XFABoFQ2xul6Qru', 2, 're', 'as', '2020-10-05 23:33:57', '2020-10-05 23:33:57');
 
 --
 -- Indexes for dumped tables
@@ -112,6 +149,12 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -125,13 +168,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `histories`
 --
 ALTER TABLE `histories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
